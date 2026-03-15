f90a853d081e798fb1a7c9876cb373eb9db87f68
sachyamani <sach.yamani@gmail.com>
2026-03-15 05:53:30 -0400

    Changes in Build CarModel and CarMake Django Models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,26 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 2
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'car_make', 'type', 'year', 'dealer_id']
+    list_filter = ['car_make', 'type', 'year']
+    search_fields = ['name']
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ['name', 'description', 'country_of_origin', 'founded_year']
+    search_fields = ['name']
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,6 +15,9 @@ urlpatterns = [
     # path for registration
     path(route='register', view=views.registration_request, name='register'),
 
+    # path to get_cars
+    path(route='get_cars', view=views.get_cars, name = 'getcars'),
+
     # path for dealer reviews view
 
     # path for add a review view
